2bf89d77e8c3dd65021822ab580c22bc3a29a2c2
mukiwu <muki.wu@goodlinker.io>
2022-07-12 16:37:26 +0800

    ✨ feat: 載入 datepicker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 import './index.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.component('date-picker', Datepicker)
+app.use(router).mount('#app')
